b77c4bf2c8430dcefc75d5196176cf9b2a77605b
Dron2019 <mr.andrey@bigmir.net>
2021-08-17 14:43:28 +0300

    conception page in progress

--- src/assets/scripts/gulp-modules/conception.js
+++ src/assets/scripts/gulp-modules/conception.js
@@ -1,32 +1,39 @@
-const tl = gsap.timeline({ paused: true })
-  .to('#circle1', {
-    duration: 5,
-    repeat: 0,
-    // yoyo: true,
-    ease: 'power1.inOut',
-    motionPath: {
-      path: '#road1',
-      align: '#road1',
-      // autoRotate: true,
-      end: 1,
-      alignOrigin: [0.5, 0.5],
-    },
-  }).to('#circle2', {
-    duration: 5,
-    repeat: 0,
-    // yoyo: true,
-    ease: 'power1.inOut',
-
-    motionPath: {
-      path: '#road2',
-      align: '#road2',
-      // autoRotate: true,
-      alignOrigin: [0.5, 0.5],
-    },
-  }, '<').progress(0);
-
-  const svg = document.querySelector('[data-motion-svg]');
+/* eslint-disable no-undef */
+const svgs = document.querySelectorAll('[data-motion-svg]');
 // locoScroll.destroy();
-svg.addEventListener('mouseenter', function () {
-  tl.restart();
+
+svgs.forEach((svg) => {
+  const circle1 = svg.querySelector('.circle1');
+  const circle2 = svg.querySelector('.circle2');
+  const road1 = svg.querySelector('.road1');
+  const road2 = svg.querySelector('.road2');
+  const tl = gsap.timeline({ paused: true })
+    .to(circle1, {
+      duration: 5,
+      repeat: 0,
+      // yoyo: true,
+      ease: 'power1.inOut',
+      motionPath: {
+        path: road1,
+        align: road1,
+        // autoRotate: true,
+        end: 1,
+        alignOrigin: [0.5, 0.5],
+      },
+    }).to(circle2, {
+      duration: 5,
+      repeat: 0,
+      // yoyo: true,
+      ease: 'power1.inOut',
+
+      motionPath: {
+        path: road2,
+        align: road2,
+        // autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+      },
+    }, '<').progress(0);
+  svg.addEventListener('mouseenter', () => {
+    tl.restart();
+  });
 });
