cc2b79b00eb7c29f48d64432ecfcec4102a31e7c
Dron2019 <mr.andrey@bigmir.net>
2021-08-25 11:20:47 +0300

    add footer

--- src/assets/scripts/gulp-modules/menu.js
+++ src/assets/scripts/gulp-modules/menu.js
@@ -83,3 +83,43 @@ callMenu.addEventListener('click', () => {
     menuTransitions.closing();
   }
 });
+
+
+const svgs = document.querySelectorAll('[data-motion-svg]');
+// locoScroll.destroy();
+
+svgs.forEach((svg) => {
+  const circle1 = svg.querySelector('.circle1');
+  const circle2 = svg.querySelector('.circle2');
+  const road1 = svg.querySelector('.road1');
+  const road2 = svg.querySelector('.road2');
+  const tl = gsap.timeline({ paused: true })
+    .to(circle1, {
+      duration: 5,
+      repeat: 0,
+      // yoyo: true,
+      ease: 'power1.inOut',
+      motionPath: {
+        path: road1,
+        align: road1,
+        // autoRotate: true,
+        end: 1,
+        alignOrigin: [0.5, 0.5],
+      },
+    }).to(circle2, {
+      duration: 5,
+      repeat: 0,
+      // yoyo: true,
+      ease: 'power1.inOut',
+
+      motionPath: {
+        path: road2,
+        align: road2,
+        // autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+      },
+    }, '<').progress(0);
+  svg.addEventListener('mouseenter', () => {
+    tl.restart();
+  });
+});
