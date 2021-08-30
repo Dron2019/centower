382d6e1f01cf3e592f9c73ecfd05c01f2ee4d56e
Dron2019 <mr.andrey@bigmir.net>
2021-08-30 09:02:04 +0300

    fix some scrollTrigger values with Loco Scroll

--- src/assets/scripts/gulp-modules/developer.js
+++ src/assets/scripts/gulp-modules/developer.js
@@ -25,19 +25,69 @@ ScrollTrigger.scrollerProxy(document.body, {
 ScrollTrigger.addEventListener('fixed', () => locoScroll.update());
 
 ScrollTrigger.refresh();
+gsap.defaults({
+  ease: 'none',
+  duration: 1,
+});
 gsap.set('.developer-img3 img', { scale: 1.2 });
+gsap.set('.developer-paragraph-with-text__img img', { scale: 1.2 });
 const dev3 = document.querySelector('.developer-img3');
 const dev3Img = dev3.querySelector('img');
 gsap.timeline({
+  ease: 'none',
   scrollTrigger: {
     trigger: dev3,
-    scrub: 0.5,
+    scrub: 0.3,
     markers: true,
-    end: '0% top',
   },
 })
   .fromTo(dev3Img, {
     y: -50,
   }, {
     y: 50,
+  })
+  .fromTo('.developer-paragraph-with-text img', {
+    y: -50,
+  }, {
+    y: 50,
+  }, '<');
+gsap.set('.developer-img2 img', { scale: 1.2 });
+gsap.timeline({
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.developer-img2',
+    scrub: 0.25,
+    markers: true,
+  },
+})
+  .fromTo('.developer-img2 img', {
+    y: -50,
+  }, {
+    y: 50,
   });
+
+gsap.timeline({
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.dev-mask2',
+    scrub: 0.25,
+    markers: true,
+  },
+})
+  .fromTo('[mask="url(#mask1)"] path', {
+    y: -50,
+  }, {
+    y: 50,
+  });
+
+
+function timelineParams(params, trigger = undefined) {
+  const initialParams = {
+    scrollTrigger: {
+      trigger,
+      scrub: 0.25,
+      markers: true,
+    },
+  };
+  return Object.assign(initialParams, params);
+};
