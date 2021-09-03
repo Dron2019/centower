59025c9de40ed2befda29e9e92210aa9772c3cd9
Dron2019 <mr.andrey@bigmir.net>
2021-09-03 10:08:02 +0300

    1920 adaptiuve start1

--- src/assets/scripts/gulp-modules/location.js
+++ src/assets/scripts/gulp-modules/location.js
@@ -3,17 +3,18 @@ function handleMapLegendOpening() {
   const legend = document.querySelector('[data-mob-accordeon]');
   const legendTop = legend.querySelector('.map__legend-title');
   const markersText = legend.querySelectorAll('.map__legend-marker-text');
+  const percentToScale = document.documentElement.clientWidth > 1680 ? 24 : 20;
   gsap.set(legendTop, { yPercent: -100 });
   gsap.set(markersText, { autoAlpha: 0 });
-  gsap.set(legend, { clipPath: 'polygon(0% 0%, 21% 0%, 20% 100%, 0% 100%)', webkitClipPath: 'polygon(0% 0%, 21% 0%, 20% 100%, 0% 100%)' });
+  gsap.set(legend, { clipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)`, webkitClipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)` });
   legend.addEventListener('mouseenter', () => {
     gsap.to(legendTop, { yPercent: 0 });
     gsap.to(markersText, { autoAlpha: 1, stagger: 0.05 });
     gsap.fromTo(
       legend,
       {
-        clipPath: 'polygon(0% 0%, 21% 0%, 20% 100%, 0% 100%)',
-        webkitClipPath: 'polygon(0% 0%, 21% 0%, 20% 100%, 0% 100%)',
+        clipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)`,
+        webkitClipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)`,
       },
       {
         clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
@@ -29,7 +30,7 @@ function handleMapLegendOpening() {
     gsap.fromTo(
       legend,
       { clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', webkitClipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' },
-      { clipPath: 'polygon(0% 0%, 21% 0%, 20% 100%, 0% 100%)', webkitClipPath: 'polygon(0% 0%, 21% 0%, 20% 100%, 0% 100%)' },
+      { clipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)`, webkitClipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)` },
     );
   });
 }
