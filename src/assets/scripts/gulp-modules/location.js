a43498fe831143bcbd49bfe44f0d9709450aa8a3
Dron2019 <55529637+Dron2019@users.noreply.github.com>
2021-09-07 14:57:43 +0300

    typical pages blocks adapted, location filter mobile handler add

--- src/assets/scripts/gulp-modules/location.js
+++ src/assets/scripts/gulp-modules/location.js
@@ -1,14 +1,25 @@
 /* eslint-disable no-undef */
 function handleMapLegendOpening() {
+  const WIDTH = document.documentElement.clientWidth;
   const legend = document.querySelector('[data-mob-accordeon]');
   const legendTop = legend.querySelector('.map__legend-title');
   const markersText = legend.querySelectorAll('.map__legend-marker-text');
-  const percentToScale = document.documentElement.clientWidth > 1680 ? 24 : 20;
+  let percentToScale = document.documentElement.clientWidth > 1680 ? 24 : 20;
+  if (WIDTH < 576) percentToScale = 0;
+  const mobileOpenFilterButton = document.querySelector('[data-mobile-filter-button]');
+  const mobileCloseFilerButton = document.querySelector('[data-mobile-close-legend]');
   gsap.set(legendTop, { yPercent: -100 });
   gsap.set(markersText, { autoAlpha: 0 });
   gsap.set(legend, { clipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)`, webkitClipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)` });
-  legend.addEventListener('mouseenter', () => {
-    // gsap.to(legendTop, { yPercent: 0 });
+  
+  if (WIDTH > 576) {
+    legend.addEventListener('mouseenter', openLegend);
+    legend.addEventListener('mouseleave', closeLegend);
+  } else {
+    mobileOpenFilterButton.addEventListener('click', openLegend);
+    mobileCloseFilerButton.addEventListener('click', closeLegend);
+  }
+  function openLegend() {
     gsap.to(markersText, { autoAlpha: 1, stagger: 0.05 });
     gsap.fromTo(
       legend,
@@ -23,15 +34,14 @@ function handleMapLegendOpening() {
         duration: 1.2,
       },
     );
-  });
-  legend.addEventListener('mouseleave', () => {
-    // gsap.to(legendTop, { yPercent: -100 });
+  }
+  function closeLegend() {
     gsap.to(markersText, { autoAlpha: 0 });
     gsap.fromTo(
       legend,
       { clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', webkitClipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' },
       { clipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)`, webkitClipPath: `polygon(0% 0%, ${percentToScale}% 0%, ${percentToScale}% 100%, 0% 100%)` },
     );
-  });
+  }
 }
 handleMapLegendOpening();
